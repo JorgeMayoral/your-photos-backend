918797dbbecd9e42cb44ecedbe626db29e46c3f2
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-08-06 19:19:54 +0200

    feat: :sparkles: Add error handleing for auth middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,45 +1,48 @@
 import { PrismaClient } from "@prisma/client";
 import { NextFunction, Response } from "express";
+import asyncHandler from "express-async-handler";
 
 import { AuthorizedRequest, DecodedToken } from "../types";
 import { validateToken } from "../utils/jwt";
 
-export const protect = async (
-  req: AuthorizedRequest,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  const prisma = new PrismaClient();
-  let token;
+export const protect = asyncHandler(
+  async (
+    req: AuthorizedRequest,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const prisma = new PrismaClient();
+    let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = validateToken(token);
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      try {
+        token = req.headers.authorization.split(" ")[1];
+        const decoded = validateToken(token);
 
-      const user = await prisma.user.findUnique({
-        where: { id: (decoded as DecodedToken).id },
-      });
+        const user = await prisma.user.findUnique({
+          where: { id: (decoded as DecodedToken).id },
+        });
 
-      if (!user) {
-        res.status(404);
-        throw new Error("User not found");
+        if (!user) {
+          res.status(404);
+          throw new Error("User not found");
+        }
+
+        req.user = user;
+
+        next();
+      } catch (error) {
+        res.status(401);
+        throw new Error("Not authorized - Token failed.");
       }
+    }
 
-      req.user = user;
-
-      next();
-    } catch (error) {
+    if (!token) {
       res.status(401);
-      throw new Error("Not authorized - Token failed.");
+      throw new Error("Not authorized - No token.");
     }
   }
-
-  if (!token) {
-    res.status(401);
-    throw new Error("Not authorized - No token.");
-  }
-};
+);
